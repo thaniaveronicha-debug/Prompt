f14fbc782cf77fec20389e736897c82b751b0d75
SK LADANG TAKU | PDT 2 <dba7361.t1@m365a1.moe.edu.my>
2026-04-13 15:31:38 +0800

    Built web
    Branch: master
    Commit: c6bb5516efe8cc5f9a419f13e46010bfab28aff2 (dirty)
    
    package:peanut 6.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "345547845" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2198284168" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
